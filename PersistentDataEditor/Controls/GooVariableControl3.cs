f00869833a072ca1915564ab997108be6978d78c
秋水 <1123993881@qq.com>
2024-06-20 09:00:23 +0800

    fix: add all python3 goo controls.
    Issues: #6

--- PersistentDataEditor/Controls/GooVariableControl3.cs
+++ PersistentDataEditor/Controls/GooVariableControl3.cs
@@ -4,6 +4,7 @@ using HarmonyLib;
 using System;
 using System.Drawing;
 using Complex = System.Numerics.Complex;
+using Rhino.Geometry;
 
 namespace PersistentDataEditor.Controls;
 internal class GooVariableControl3 : GooHorizonalControlBase<IGH_Goo>
@@ -43,7 +44,7 @@ internal class GooVariableControl3 : GooHorizonalControlBase<IGH_Goo>
         {
             return [new GooBooleanControl(() => (GH_Boolean)ShowValue, _isNull)];
         }
-        if (type == typeof(int) && Data.UseParamIntegerControl)
+        else if (type == typeof(int) && Data.UseParamIntegerControl)
         {
             return [
                 new GooIntegerControl(() =>
@@ -66,11 +67,11 @@ internal class GooVariableControl3 : GooHorizonalControlBase<IGH_Goo>
                     }
                 }, _isNull, null)];
         }
-        if (type == typeof(string) && Data.UseParamStringControl)
+        else if (type == typeof(string) && Data.UseParamStringControl)
         {
             return [new GooInputBoxStringControl<GH_String>(() => (GH_String)ShowValue, _isNull)];
         }
-        if (type == typeof(double) && Data.UseParamNumberControl)
+        else if (type == typeof(double) && Data.UseParamNumberControl)
         {
             return [new GooNumberControl(() =>
                 {
@@ -92,13 +93,53 @@ internal class GooVariableControl3 : GooHorizonalControlBase<IGH_Goo>
                     }
                 }, _isNull, null)];
         }
-        if (type == typeof(Complex) && Data.UseParamComplexControl)
+        else if (type == typeof(Complex) && Data.UseParamComplexControl)
         {
             return [new GooComplexControl(() => (GH_ComplexNumber)ShowValue, _isNull, null)];
         }
-        if (type == typeof(Color) && Data.UseParamColourControl)
+        else if (type == typeof(Color) && Data.UseParamColourControl)
         {
             return [new GooColorControl(() => (GH_Colour)ShowValue, _isNull)];
+        }
+        else if (type == typeof(Point3d) && Data.UseParamPointControl)
+        {
+            return [new GooPointControl(() => (GH_Point)ShowValue, _isNull, null)];
+        }
+        else if (type == typeof(Vector3d) && Data.UseParamVectorControl)
+        {
+            return [new GooVectorControl(() => (GH_Vector)ShowValue, _isNull, null)];
+        }
+        else if (type == typeof(Plane) && Data.UseParamPlaneControl)
+        {
+            return [new GooPlaneControl(() => (GH_Plane)ShowValue, _isNull, null)];
+        }
+        else if (type == typeof(Interval) && Data.UseParamDomainControl)
+        {
+            return [new GooIntervalControl(() => (GH_Interval)ShowValue, _isNull, null)];
+        }
+        else if (type == typeof(UVInterval) && Data.UseParamDomain2Control)
+        {
+            return [new GooInterval2DControl(() => (GH_Interval2D)ShowValue, _isNull, null)];
+        }
+        else if (type == typeof(Box) && Data.UseParamBoxControl)
+        {
+            return [new GooBoxControl(() => (GH_Box)ShowValue, _isNull, null)];
+        }
+        else if (type == typeof(Line) && Data.UseParamLineControl)
+        {
+            return [new GooLineControl(() => (GH_Line)ShowValue, _isNull, null)];
+        }
+        else if (type == typeof(Circle) && Data.UseParamCircleControl)
+        {
+            return [new GooCircleControl(() => (GH_Circle)ShowValue, _isNull, null)];
+        }
+        else if (type == typeof(Arc) && Data.UseParamArcControl)
+        {
+            return [new GooArcControl(() => (GH_Arc)ShowValue, _isNull, null)];
+        }
+        else if (type == typeof(Rectangle3d) && Data.UseParamRectangleControl)
+        {
+            return [new GooRectangleControl(() => (GH_Rectangle)ShowValue, _isNull, null)];
         }
         return [new GooGeneralControl<IGH_Goo>(() => ShowValue, _isNull)];
     }
